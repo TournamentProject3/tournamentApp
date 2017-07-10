e40fb6f49b9956057a91d439b5b8cdfb04caf05e
DESKTOP-J3CNHG3\Suleyman <suleymanogut38@gmail.com>
2017-07-10 23:37:14 +0300

    null edittext field errors added.

--- app/src/main/java/com/example/irem/tournament/EliminationMode.java
+++ app/src/main/java/com/example/irem/tournament/EliminationMode.java
@@ -12,89 +12,50 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-
-
-
 /**
  * Created by irem on 20.06.2017.
  */
 
 public class EliminationMode extends Activity {
 
-
     private EditText txtTournamentName, txtParticipantsNumber;
     private Button btnAddParticipants;
     private LinearLayout participantsLayout;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eliminationmode);
-
-
-
-
         btnAddParticipants = (Button) findViewById(R.id.btnAdd);
 
         txtTournamentName = (EditText) findViewById(R.id.edtTextTournamentName);
         txtParticipantsNumber = (EditText) findViewById(R.id.edtTextCount);
-       // participantsLayout = (LinearLayout) findViewById(R.id.linearlayout);
-
 
         btnAddParticipants.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Editable tournamentName = txtTournamentName.getText();
-                //int participantsNumber = Integer.parseInt(txtParticipantsNumber.getText().toString());
+
+                if(txtTournamentName.getText().toString().trim().equals("") )
+                {
+                    Toast.makeText(getApplicationContext(),"please enter the tournament name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (txtParticipantsNumber.getText().toString().trim().equals("")){
+                    Toast.makeText(getApplicationContext(), "Please enter value between 4 and 64!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String val = txtParticipantsNumber.getText().toString();
+                int partCountVal = Integer.parseInt(val);
+                if(4>partCountVal || partCountVal>64){
+                    Toast.makeText(getApplicationContext(), "Please enter value between 4 and 64!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 Intent intent = new Intent(EliminationMode.this, recyclerView.class);
                 intent.putExtra("count", txtParticipantsNumber.getText().toString());
                 startActivity(intent);
-
-
-                //createParticipants(participantsNumber);
             }
         });
-
-
-
-    /*public void createParticipants(int numOfParticipants) {
-
-        final EditText[] myEditTextViews = new EditText[numOfParticipants]; // create an empty array;
-
-        for (int i = 0; i < numOfParticipants; i++) {
-            // create a new textview
-            final EditText rowEditView = new EditText(this);
-            // set some properties of rowTextView or something
-            rowEditView.setText("This is row #" + i);
-            // add the textview to the linearlayout
-            participantsLayout.addView(rowEditView);
-
-            // save a reference to the textview for later
-            myEditTextViews[i] = rowEditView;
-            myEditTextViews[i].setVisibility(View.VISIBLE);
-
-            ((ViewGroup)participantsLayout.getParent()).removeView(participantsLayout);
-            setContentView(participantsLayout);
-
-
-
-
-        }
-        class JavaLayoutActivity extends Activity {
-
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                Button myButton = new Button(this);
-                RelativeLayout myLayout = new RelativeLayout(this);
-                myLayout.addView(myButton);
-                setContentView(myLayout);
-
-            }
-        }*/
     }
 }
 
